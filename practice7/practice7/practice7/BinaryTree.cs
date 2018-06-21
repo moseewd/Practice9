31a46e472ce6476d355558c791d68522d3e63929
Danila <moseewd@gmail.com>
2018-06-22 02:48:24 +0500

    //

---
+++ practice7/practice7/practice7/BinaryTree.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice9
+{
+    public class CircleList<T>
+    {
+
+        Node<T> head; // головной/первый элемент
+        Node<T> tail; // последний/хвостовой элемент
+        int count;  // количество элементов в списке
+
+        // добавление элемента
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            // если список пуст
+            if (head == null)
+            {
+                head = node;
+                tail = node;
+                tail.Next = head;
+            }
+            else
+            {
+                node.Next = head;
+                tail.Next = node;
+                tail = node;
+            }
+            count++;
+        }
+
+        /// <summary>
+        /// Возврощаем первый условные элемент циклического списка
+        /// </summary>
+        /// <returns>Первый условный элемент</returns>
+        public Node<T> GetHead()
+        {
+            return head;
+        }
+
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public bool Contains(T data)
+        {
+            Node<T> current = head;
+            if (current == null) return false;
+            do
+            {
+                if (current.Data.Equals(data))
+                    return true;
+                current = current.Next;
+            }
+            while (current != head);
+            return false;
+        }
+
+
+    }
+
+
+
+    public class Node<T>
+    {
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+    }
+}
---
+++ practice7/practice7/practice7/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice9
+{
+    class Program
+    {
+        public static int ReadNatural()
+        //ввод числа для красивых лаб
+        {
+            bool check = false;
+            int intNum=0;
+            do
+            {
+                // Попытка перевести строку в число
+                check = Int32.TryParse(Console.ReadLine(), out intNum);
+                // Если попытка неудачная:
+                if (!(check)|| (intNum < 0))
+                    Console.WriteLine("Некорректный ввод. Попробуйте ещё раз");
+            } while (!(check)||(intNum<0));
+            // Если попытка удачная:
+            return intNum;
+        }
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            CircleList<int> circleList = new CircleList<int>();
+            Console.WriteLine("Введите количество элементов которые необходимо добавить в список");
+            int n = ReadNatural();
+            for (int i = 0; i < n; i++)
+            {
+                circleList.Add(rnd.Next(-100, 101));
+            }
+            int sumplus=0;
+            int summinus=0;
+
+            Console.WriteLine("Текущий список:");
+            Node<int> point = circleList.GetHead();
+            for (int i = 0; i < circleList.Count; i++)
+            {
+                Console.Write( point.Data + " ");
+                if (point.Data > 0)
+                    sumplus += point.Data;
+                else
+                    summinus += point.Data;
+
+                point = point.Next;
+            }
+
+            Console.WriteLine("Сумма всех положительных элементов={0}", sumplus);
+            Console.WriteLine("Сумма всех отрицательных элементов={0}", summinus);
+        }
+    }
+}
